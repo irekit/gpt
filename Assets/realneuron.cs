cedab671d348cf7efcefaa73ff20dcece09c4dfd
IREKIT <177529071+irekit@users.noreply.github.com>
2026-06-26 22:17:15 +1000

    added stuff

--- Assets/realneuron.cs
+++ Assets/realneuron.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 public class realneuron : MonoBehaviour
 {
     [SerializeField] private realneuron[] passon;
@@ -15,15 +16,22 @@ public class realneuron : MonoBehaviour
     [SerializeField] private float resting_volt = 0;
     [SerializeField] private GameObject aura;
     [SerializeField] private GameObject circleglow;
+    [SerializeField] private float sensitivity = 2;
     private SpriteRenderer circleglowrender;
     [SerializeField] private SpriteRenderer sprend;
     private lens master;
     public float ininc = 0;
     private InputAction scroll;
+    float aiset = 1;
     [SerializeField] private InputActionAsset inp;
+    [SerializeField] private float scalmult = 0.01f;
+    [SerializeField] private float voltspeed = 0.05f;
+    [SerializeField] private float incoming_multiplier = 0.03f;
+    [SerializeField] private bool l1neuron = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Application.targetFrameRate = 60;
         wai = new WaitForSeconds(0.01f);
         blocks = new MaterialPropertyBlock[axons.Length];
         for(int i = 0; i < axons.Length; i++)
@@ -39,45 +47,48 @@ public class realneuron : MonoBehaviour
     float startsuppress = 0;
     [SerializeField]bool first_neuron = false;
     [SerializeField] private GameObject electric;
+    [SerializeField] private float startsupps = 0.01f;
     // Update is called once per frame
+    bool startt = false;
     void Update()
     {
         Vector2 del = scroll.ReadValue<Vector2>();
         Vector2 mp = Mouse.current.position.ReadValue();
         Vector3 mousepos = Camera.main.ScreenToWorldPoint(new Vector3(mp.x, mp.y, 10f));
-        if (master.started && startsuppress <= 0)
+        if (master.started && startsuppress <= 0 && !(startt && l1neuron))
         {
-            incoming = ininc;
-            voltage = 0;
-            startsuppress = 1;
+            incoming += ininc;
+            startsuppress = startsupps;
+            aiset = 1 + (Random.Range(0, 2) * 2 - 1) * 0.25f;
         }
         if(startsuppress > 0)
         {
             startsuppress -= Time.deltaTime;
         }
+        if(aiset < 0.975f)
+        {
+            aiset += 0.015f;
+        }
+        else if(aiset > 1.025f)
+        {
+            aiset -= 0.015f;
+        }
+        else
+        {
+            aiset = 1;
+        }
         if (!spiking)
         {
             if (incoming > 0.05f || incoming < -0.05f)
             {
-                float inc = incoming * 0.01f;
+                float inc = incoming * incoming_multiplier;
                 voltage += inc;
                 incoming -= inc;
             }
             else
             {
                 incoming = 0;
-                if (voltage < resting_volt - 0.1f)
-                {
-                    voltage += 0.005f;
-                }
-                else if (voltage > resting_volt + 0.1f)
-                {
-                    voltage -= 0.005f;
-                }
-                else
-                {
-                    voltage = resting_volt;
-                }
+                voltage = Mathf.Lerp(voltage, resting_volt, voltspeed);
             }
             
         }
@@ -87,36 +98,41 @@ public class realneuron : MonoBehaviour
             voltage = threshold;
             StartCoroutine(Fire());
         }
-        float avoltage = Mathf.Clamp(voltage + incoming, -2, 1000000);
-        float sca = Mathf.Max(voltage * 0.05f + 1.5f, 1.5f);
-        float lightness = Mathf.Clamp(0.75f+avoltage*0.05f, 0.1f, 1);
-        float scaura = Mathf.Max(Mathf.Log(0.053f*avoltage - 0.045f) * 0.95f + 3, 0);
-        aura.transform.localScale = new Vector3(scaura, scaura, 1);
-        sprend.color = new Color(lightness, lightness, lightness, 1);
-        transform.localScale = new Vector3(sca, sca, 1);
         if (!spiking)
         {
+
+            float avoltage = Mathf.Clamp(voltage + incoming, -2, 1000000);
+            float sca = Mathf.Max(voltage * 0.05f + 1.5f, 1.5f);
+            float lightness = Mathf.Clamp(0.75f + avoltage * 0.05f, 0.1f, 1);
+            float scaura = Mathf.Max(Mathf.Log(0.053f * avoltage - 0.045f) * 0.95f + 3, 0);
+            aura.transform.localScale = new Vector3(scaura, scaura, 1);
+            sprend.color = new Color(lightness, lightness, lightness, 1);
+            transform.localScale = new Vector3(sca, sca, 1);
             for (int i = 0; i < axons.Length; i++)
             {
                 blocks[i].SetFloat("_lerpos", -10);
                 axons[i].SetPropertyBlock(blocks[i]);
             }
         }
-        Debug.Log(mousepos);
         if (Vector3.Distance(transform.position, mousepos) < 1.2f && first_neuron)
         {
-            ininc += del.y * 2f;
+            ininc += del.y * sensitivity;
             ininc = Mathf.Clamp(ininc, 0, 75);
         }
         if (first_neuron)
         {
-            electric.transform.localScale = new Vector3(electric.transform.localScale.x, ininc * 0.01f, 1);
+            electric.transform.localScale = new Vector3(electric.transform.localScale.x, ininc * scalmult * aiset, 1);
 
         }
     }
     WaitForSeconds wai;
     IEnumerator Fire()
     {
+        if (l1neuron && !startt)
+        {
+            startt = true;
+        }
+        //neuron firin
         circleglow.SetActive(true);
         circleglowrender.color = new Color(1, 1, 1, 0.1f);
         circleglow.transform.localScale = new Vector3(0.51f, 0.51f, 1);
@@ -124,35 +140,38 @@ public class realneuron : MonoBehaviour
         voltage = threshold;
         float plac = 0;
         
-        for(int i = 0; i < 8; i++)
+        for(int i = 0; i < 16; i++)
         {
-            voltage += 1f;
-            circleglowrender.color = new Color(1, 1, 1, i * 0.125f + 0.125f);
-            yield return wai;
+            voltage += 4f;
+            circleglowrender.color = new Color(1, 1, 1, i * 0.0625f + 0.0625f);
+            yield return null;
         }
-        yield return null;
-        yield return null;
-        yield return null;
-        voltage = threshold + 1;
-        for (int i = 0; i < 10; i++)
+        for(int i = 0; i < 3; i++)
         {
-            voltage -= (float)(threshold + 5) / 10f;
-            circleglow.transform.localScale = new Vector3(0.51f + i * 0.1f, 0.51f + i * 0.1f, 1);
-            circleglowrender.color = new Color(1, 1, 1, 0.9f - i * 0.1f);
+            voltage -= 20f;
+            yield return null;
+        }
+        voltage = threshold + 1;
+        for (int i = 0; i < 20; i++)
+        {
+            voltage -= (float)(threshold + 20) / 20f;
+            circleglow.transform.localScale = new Vector3(0.505f + i * 0.05f, 0.505f + i * 0.05f, 1);
+            circleglowrender.color = new Color(1, 1, 1, 0.95f - i * 0.05f);
             for (int j = 0; j < axons.Length; j++)
             {
                 blocks[j].SetFloat("_lerpos", plac);
                 axons[j].SetPropertyBlock(blocks[j]);
             }
             plac += 0.1f;
-            yield return wai;
+            yield return null;
         }
+        
         for (int j = 0; j < axons.Length; j++)
         {
             blocks[j].SetFloat("_lerpos", -100);
             axons[j].SetPropertyBlock(blocks[j]);
         }
-        voltage = -1;
+        voltage = -10;
         for(int i = 0; i < passon.Length; i++)
         {
             passon[i].incoming += weights[i];
--- Assets/voltometer.cs
+++ Assets/voltometer.cs
@@ -7,15 +7,17 @@ public class voltometer : MonoBehaviour
     [SerializeField] private realneuron neuronscript;
     Vector3[] positions;
     private lens master;
+    [SerializeField] private float scal = 0.007f;
+    float summary = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         linerend = GetComponent<LineRenderer>();
 
-        positions = new Vector3[20];
-        for(int i = 0; i < 20; i++)
+        positions = new Vector3[60];
+        for(int i = 0; i < 60; i++)
         {
-            positions[i] = new Vector3(-1.1f + i * 0.115f, 0, -1);
+            positions[i] = new Vector3(-1.1f + i * 0.037f, 0, -1);
         }
         master = GameObject.FindGameObjectWithTag("Player").GetComponent<lens>();
     }
@@ -25,29 +27,29 @@ public class voltometer : MonoBehaviour
     {
         if (master.started)
         {
-            for(int i = 0; i < 20; i++)
-            {
-                positions[i].y = 0;
-            }
+            //for(int i = 0; i < 40; i++)
+            //{
+              //  positions[i].y = 0;
+            //}
         }
-        if (et == 10)
+        summary = Mathf.Lerp(summary, neuronscript.voltage, 1f);
+        if (et == 3)
         {
             et = 0;
-            for (int i = 0; i < 19; i++)
+            for (int i = 0; i < 59; i++)
             {
                 positions[i].y = positions[i + 1].y;
             }
-            positions[19].y = neuronscript.voltage * 0.05f;
-            Vector3[] vecs = new Vector3[20];
-            for (int i = 0; i < 20; i++)
+            
+            positions[59].y = summary * scal;
+
+            Vector3[] vecs = new Vector3[60];
+            for (int i = 0; i < 60; i++)
             {
                 vecs[i] = transform.TransformPoint(positions[i]);
             }
             linerend.SetPositions(vecs);
         }
-        else
-        {
-            et++;
-        }
+        et++;
     }
 }
